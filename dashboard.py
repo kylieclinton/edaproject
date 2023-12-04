a093710421a4306d2020d7cc489646b3a0420b1f
Kylie Clinton <kcc91600@byu.edu>
2023-12-04 15:26:06 -0700

    update streamlit

--- dashboard.py
+++ dashboard.py
@@ -2,16 +2,37 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import plotly.express as px
+# import seaborn as sns
+# import matplotlib.pyplot as plt
 
 marvel_df= pd.read_csv('marvel_clean.csv')
 og_df = pd.read_csv('marvel.csv')
 years_df = pd.read_csv('years_df.csv')
 
+years_df.set_index('name', inplace=True)
+
 st.title('Marvel Comics Dashboard')
 
-selected_character = st.selectbox('Select a character', og_df['name'])
+selected_character = st.selectbox('Select a character', years_df.index)
 
-name_df = marvel_df[marvel_df['name'] == selected_character]
-st.dataframe(name_df)
+st.header(selected_character)
+character = years_df.loc[selected_character].cumsum()
+character.reset_index(drop = True, inplace=True)
 
-st.dataframe(marvel_df)
+# cumulative_sum = character.cumsum()
+tidy_df = pd.DataFrame({
+    'Year': character.index,
+    'Mentions': character.values
+})
+
+fig = px.line(tidy_df, x='Year', y='Mentions',
+              labels={"Mentions": "Mentions", "Year": "Year"},
+              title=f'{selected_character} Mentions Over Time in Comics',
+              line_shape="linear", width=800, height=500)
+
+# # Customize the layout
+fig.update_layout(xaxis=dict(tickangle=45, tickmode='linear', tick0=min(tidy_df['Year']), dtick=5, tickvals=list(range(1939, 2024, 5))))
+# Show the plot in Streamlit
+
+st.plotly_chart(fig)
+st.dataframe(years_df.index)
