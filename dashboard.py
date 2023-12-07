68f5efe3c0dcf9a40a5ad22248bcdbde94b24219
Kylie Clinton <kcc91600@byu.edu>
2023-12-07 15:13:00 -0700

    update dashboard year info

--- dashboard.py
+++ dashboard.py
@@ -26,12 +26,21 @@ colors = ['#d0384e',
 
 years_df.set_index('name', inplace=True)
 
+#add columns to years_df for the years 1942,1943,1944,1946,1947,1956,1957,1958,1960,1969,1980, that aren't mentioned with all values 0
+#add new columns to dataframe for each year
+years_df = years_df.reindex(columns=years_df.columns.tolist() + ['1942', '1943', '1944', '1946', '1947', '1956', '1957', '1958', '1960', '1969', '1980'], fill_value=0)
+#order the columns in ascending numerical order for the years
+years_df = years_df.reindex(sorted(years_df.columns), axis=1)
+# filter years_df to only include characters with more than 10 mentions
+new_years_df = years_df[years_df.sum(axis=1) > 15]
+
+# st.dataframe(years_df)
 st.title('Marvel Comics Dashboard')
 st.write("This dashboard provides insights into the popularity of Marvel comics characters over time. Select characters and years to learn about their popularity over time")
-selected_character = st.selectbox('Select a character', years_df.index)
+selected_character = st.selectbox('Select a character (type name for specific character)', new_years_df.index)
 
 st.header(selected_character)
-character = years_df.loc[selected_character]
+character = new_years_df.loc[selected_character]
 cumulative_sum = character.cumsum()
 cumulative_sum.reset_index(drop = True, inplace=True)
 tidy_df = pd.DataFrame({
@@ -39,12 +48,50 @@ tidy_df = pd.DataFrame({
     'Mentions': cumulative_sum.values
 })
 
-fig = px.line(tidy_df, x='Year', y='Mentions',
-              labels={"Mentions": "Mentions", "Year": "Year"},
-              title=f'Cumulative Comic Book Appearances for {selected_character}',
-              line_shape="linear", width=800, height=500, color_discrete_sequence=colors)
+unique_years = tidy_df['Year'].unique().tolist()
+start_year = 1939
 
-# # Customize the layout
+# Rename the tick labels based on the year progression
+tick_labels = [str(start_year + tick) for tick in unique_years]
+
+# Create a Plotly figure using graph_objects
+fig = go.Figure()
+
+# Add a line trace
+fig.add_trace(go.Scatter(x=tidy_df['Year'], y=tidy_df['Mentions'],
+                         mode='lines',
+                         name=f'Cumulative Mentions for {selected_character}',
+                         line=dict(color=colors[0])))
+
+# Update x-axis ticks
+fig.update_xaxes(tickmode='array', tickvals=unique_years, ticktext=tick_labels, dtick=5)
+
+# Customize the layout
+fig.update_layout(
+    title=f'Cumulative Comic Book Appearances for {selected_character}',
+    xaxis=dict(title='Year'),
+    yaxis=dict(title='Mentions'),
+    width=1000,
+    height=500
+)
+
+# Display the figure in Streamlit
+st.plotly_chart(fig)
+
+# unique_years = tidy_df['Year'].unique().tolist()
+# start_year = 1939
+
+# # Rename the tick labels based on the year progression
+# tick_labels = [str(start_year + tick) for tick in unique_years]
+
+# fig = px.line(tidy_df, x='Year', y='Mentions',
+#               labels={"Mentions": "Mentions", "Year": "Year"},
+#               title=f'Cumulative Comic Book Appearances for {selected_character}',
+#               line_shape="linear", width=800, height=500, color_discrete_sequence=colors)
+
+# fig.update_xaxes(tickmode='array', tickvals=unique_years, ticktext=tick_labels)
+
+# # # Customize the layout
 # fig.update_layout(xaxis=dict(tickmode='auto'),     annotations=[
 #         dict(
 #             x=0,  # Adjust this value to the x-coordinate where you want the note
@@ -56,36 +103,34 @@ fig = px.line(tidy_df, x='Year', y='Mentions',
 #             ax=0,
 #             ay=-40
 #         )
-    # ])
+#     ])
 
-tickval = list(years_df.columns)
-fig.update_layout(xaxis=dict(tickmode='auto'),annotations=[dict(ticktext = ["1939",
-"1949","1959","1969","1979","1989","1999","2009","2019"
-])])
+# tickval = list(years_df.columns)
 
-st.plotly_chart(fig)
-# st.dataframe(years_df.index)
-st.write(f"Total mentions for {selected_character}: {cumulative_sum.iloc[-1]}")
-st.write(f"Year {selected_character} was mentioned the most: {character.idxmax()}")
-st.write(f"Number of mentions in {character.idxmax()}: {character.max()}")
-#top 5 characters for a selected year and total mentions
-st.text(" ")
-st.text(" ")
-#make a slider for selected year
-selected_year = st.select_slider('Select a year', years_df.columns)
+# fig.update_xaxes(tickvals=tickval, ticktext = tickval)
 
-selected_year_df = years_df[selected_year].sort_values(ascending=False)[:5]
-st.subheader(f"Top 5 characters for {selected_year}:")
-st.dataframe(selected_year_df, width=450)
-st.caption("Note: Year column shows mentions in that year for the character")
-#make the dataframe larger so the whole name shows
+# st.plotly_chart(fig)
+# # st.dataframe(years_df.index)
+# st.write(f"Total mentions for {selected_character}: {cumulative_sum.iloc[-1]}")
+# st.write(f"Year {selected_character} was mentioned the most: {character.idxmax()}")
+# st.write(f"Number of mentions in {character.idxmax()}: {character.max()}")
+# #top 5 characters for a selected year and total mentions
+# st.text(" ")
+# st.text(" ")
+# selected_year = st.select_slider('Select year', years_df.columns)
+
+# selected_year_df = years_df[selected_year].sort_values(ascending=False)[:5]
+# st.subheader(f"Top 5 characters for {selected_year}:")
+# st.dataframe(selected_year_df, width=450)
+# st.caption("Note: Year column shows mentions in that year for the character")
+# #make the dataframe larger so the whole name shows
 
 
 #comic mentions barplot for selected character(s)
 #use selection checkbox
 st.text(" ")
 st.text(" ")
-selected_characters = st.multiselect('Select characters', marvel_df['name'])
+selected_characters = st.multiselect('Select characters (type name for specific characters)', marvel_df['name'])
 
 st.subheader('Character Mentions in Comics')
 # st.bar_chart(marvel_df[marvel_df['name'].isin(selected_characters)]['comics_available'].astype(int))
